c15d801533ac970ab4d8ee5d27d194d9fc52617e
NinaMwangi <n.mwangi@alustudent.com>
2025-03-17 12:35:24 +0300

    3NF

--- Database/store_procedure.sql
+++ Database/store_procedure.sql
@@ -12,6 +12,6 @@ BEGIN
     VALUES (n_movie_id, n_title, n_movie_type, n_netflix_exclusive, n_release_date);
 END $$
 
-DELIMETER;
+DELIMITER ;
 
 CALL AddMovie(644, 'Glass Onion', 'Movie', 'Yes', '2022-09-10');
